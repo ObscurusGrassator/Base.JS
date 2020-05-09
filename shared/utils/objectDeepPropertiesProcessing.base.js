ea60037b00ea8c9cbf675b9a2858d9841e7842e7
ObscurusGrassator <obscurus.grassator@gmail.com>
2020-05-09 17:49:24 +0200

    dep: util.objectDeepPropertiesProcessing(), please use: deepPropertiesProcessing()

---
+++ shared/utils/objectDeepPropertiesProcessing.base.js
@@ -0,0 +1,28 @@
+/**
+ * Deep object properties processing.
+ * 
+ * @template {Object} T
+ * 
+ * @param {T} obj
+ * @param {function(any, string|number, (String | Number)[]): void} deepFunction deepFunction(objectPart, key, objectPartPath)
+ * 
+ * @example objectDeepPropertiesProcessing(obj, (o, i) => { if (o[i] === 0) o[i] = false; });
+ * 
+ * @returns {T} Modified obj
+ */
+function objectDeepPropertiesProcessing(obj, deepFunction) {
+	const deep = (obj, deepFunction, paths = []) => {
+		// If the user deletes from the array, we must delete from the end
+		obj && typeof obj === 'object' && Object.keys(obj).reverse().forEach(i => {
+			if (obj[i] && typeof obj[i] === 'object') {
+				deep(obj[i], deepFunction, paths.concat([Array.isArray(obj) ? +i : i]));
+			}
+			// Changes at higher levels must be made after changes at lower levels so that mapping is not lost.
+			deepFunction(obj, i, paths);
+		});
+		return obj;
+	};
+	return deep(obj, deepFunction);
+};
+
+module.exports = objectDeepPropertiesProcessing;
